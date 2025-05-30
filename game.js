9d7ad8c639c223997b8d1e6b35f9ba2b59c49001
Rohan Shergill <rohan.shergill1875@gmail.com>
2025-05-30 05:46:38 +0100

    feat: add fish eats bubble game with score counter

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    backgroundColor: "#1e1e1e",
+    backgroundColor: "0f5e9c",
     physics: {
       default: "arcade",
       arcade: {
@@ -10,6 +10,7 @@ const config = {
       }
     },
     scene: {
+      preload,
       create,
       update
     },
@@ -19,25 +20,74 @@ const config = {
       }
   };
   
-  let box;
+  let fish;
   let keys;
-  
+  let bubble;
+  const speed = 200;
+  let score = 0;
+  let scoreCounter;
+
+  function preload() {
+    this.load.image('fish', 'assets/fish_green.png');
+    this.load.image("bubble", "assets/bubble_a.png");
+  }
+
+
   function create() {
-    box = this.add.rectangle(400, 300, 50, 50, 0xff0000);
-    this.physics.add.existing(box);
-    box.body.setCollideWorldBounds(true);
-  
+    score = 0;
+    scoreCounter = this.add.text(300, 0, score, { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' });
+
+
+    fish = this.physics.add.sprite(100, 300, 'fish');
+    this.physics.add.existing(fish);
+    fish.body.setCollideWorldBounds(true);
+
+    bubble = this.physics.add.sprite(700, 400, "bubble");
+    this.physics.add.existing(bubble);
+    //bubble.body.setCollideWorldBounds(true);
+    
+    bubble.body.setVelocityX(-speed);
     //keys = this.input.keyboard.addKeys("W,A,S,D");
   }
   
   function update() {
-    const speed = 200;
-    box.body.setVelocityY(speed);
+    
+    fish.body.setVelocityY(speed);
     // Go up on press
     if (this.input.activePointer.isDown) {
-        box.body.setVelocityY(-400);
+        fish.body.setVelocityY(-400);
       }
+    updatebubble();
+    
+    this.physics.collide(bubble, fish, collisionCallback);
+
   }
   
+
+  function updatebubble(){
+    if (bubble.x < 0) {
+        bubble.x = 830;
+        bubble.y =  Math.random() * (config.height - bubble.height);
+        console.log(bubble.x, bubble.y, bubble.height);
+    }
+  }
+
+  function collisionCallback(){
+    console.log("collision detected");
+    
+    bubble.x = 830;
+        bubble.y =  Math.random() * (config.height - bubble.height);
+        console.log(bubble.x, bubble.y, bubble.height);
+    bubble.setVelocityX(-speed);
+    bubble.setVelocityY(0);
+
+    fish.setVelocityX(0);
+    fish.setVelocityY(0);
+    fish.x = 100;
+
+    score += 1;
+    scoreCounter.setText(score);
+  }
+
   new Phaser.Game(config);
   
